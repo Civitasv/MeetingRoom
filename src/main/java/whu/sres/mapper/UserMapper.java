0b879d8ab186763909ab8508c6447e1e4b56355f
Civitasv <sen.hu@whu.edu.cn>
2021-05-11 14:15:24 +0800

    解决版本缓存问题

--- src/main/java/whu/sres/mapper/UserMapper.java
+++ src/main/java/whu/sres/mapper/UserMapper.java
@@ -19,6 +19,8 @@ public interface UserMapper {
 
     int updatePwd(User user);
 
+    int updateId(String id, String fill);
+
     int updatePwdAndPhone(User user);
 
     int updatePhone(User user);
